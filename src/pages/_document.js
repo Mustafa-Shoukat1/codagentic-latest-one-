f6b6094b1e285bcd935b09b04bb5ecd62a4c45db
Bazla-25 <hafizabazlarashid@gmail.com>
2025-12-10 02:23:38 +0500

    refactor: remove obsolete Google site verification and update script placements in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,23 +4,48 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Search Console */}
+        {/* 1. Google Search Console - Already correct */}
         <meta name="google-site-verification" content="gTmNTeNp3OBAn-7AXm3kPr0KcSc8Yl3zUU7KBnCiBhA" />
-
-        {/* Google Tag Manager */}
+        {/* 2. Google Tag Manager - Head part (correct placement) */}
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');`,
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-XXXXXXX'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-XXXXXXX');`,
           }}
-          
         />
+
+        {/* 3. Meta Pixel (Facebook Pixel) - Base code in <head> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', 'YOUR_FACEBOOK_PIXEL_ID');
+            fbq('track', 'PageView');`,
+          }}
+        />
+
+        {/* Optional: Add noscript fallback for Meta Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=YOUR_FACEBOOK_PIXEL_ID&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </Head>
+
       <body>
-        {/* GTM noscript */}
+        {/* 4. Google Tag Manager - noscript part (must be right after <body>) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
